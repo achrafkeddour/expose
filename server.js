e1a5581b4748acde73ad1d62d980093ec013672d
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-02-15 15:48:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 10000;
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Read the HTML form
+const formHTML = fs.readFileSync('form1.html', 'utf8');
+
 // Serve the HTML form
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/form1.html');
+  res.send(formHTML);
 });
 
 // Handle form submission
 app.post('/', (req, res) => {
   const formData = req.body;
-  res.send(`
-    <p>Votre Nom est : ${formData.name}</p>
-    <p>Votre Age est : ${formData.age}</p>
+  const additionalDataHTML = `
+    <p>Votre Nom de famille est : ${formData.name}</p>
     <p>Votre Spécialité est : ${formData.speciality}</p>
-    <p>Votre E-mail est : ${formData.email}</p>
-  `);
+  `;
+  const responseHTML = `
+    ${formHTML.replace('</div>', `
+      ${additionalDataHTML}</div>`)}
+  `;
+  res.send(responseHTML);
 });
 
 app.listen(port, '0.0.0.0', () => {
